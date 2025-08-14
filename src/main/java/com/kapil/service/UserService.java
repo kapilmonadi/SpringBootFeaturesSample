7fc9007ae4cdd25456edb3fc70dad4e2371e5e96
kaplimonadi <kapilmonadi@gmail.com>
2025-08-14 19:06:30 +0530

    updated UserService

--- src/main/java/com/kapil/service/UserService.java
+++ src/main/java/com/kapil/service/UserService.java
@@ -2,11 +2,17 @@ package com.kapil.service;
 
 import com.kapil.dto.UserRecord;
 import com.kapil.exception.UserFetchException;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 
 public interface UserService {
 
-    @Retryable(retryFor = {UserFetchException.class})
+    @Retryable(retryFor = {UserFetchException.class},
+    maxAttempts = 3,
+    backoff = @Backoff(delay = 2000))
     UserRecord getUser(Long userId);
+
+    @Recover
     String getUserFailureMessage();
 }
