5aaa0f003f9bc6924a27e630c4d43c0e94c6a376
kaplimonadi <kapilmonadi@gmail.com>
2025-09-18 11:41:30 +0530

    changes for OpenAPI and swagger

---
+++ src/main/java/com/kapil/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.kapil.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "REST APIs", version = "1.0",
+        description = "REst APis being offered by the Sample spring Boot Application",
+        contact = @Contact(name = "Kaps")),
+        security = {@SecurityRequirement(name = "bearerToken")}
+)
+@SecuritySchemes({
+        @SecurityScheme(name = "bearerToken", type = SecuritySchemeType.HTTP,
+                scheme = "bearer", bearerFormat = "JWT")
+})
+public class OpenApiConfig {
+
+}
